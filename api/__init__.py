d2dbc0ee7da27ccfd4c9fc503666024677a7db34
Jie <mjunzh@gmail.com>
2026-04-11 23:19:26 +0800

    feat: Remove HTTP API package initialization code and related imports

--- api/__init__.py
+++
@@ -1,9 +0,0 @@
-"""HTTP API package."""
-
-try:  # pragma: no cover - allow importing without FastAPI installed
-    from .server import app
-except ModuleNotFoundError:  # pragma: no cover - testing scaffold
-    app = None  # type: ignore
-    __all__ = []
-else:
-    __all__ = ["app"]
